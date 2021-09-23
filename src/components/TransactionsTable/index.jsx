0c6625851659e8506a12e7fe5a0d028ace8971fc
Pedro Matos <pedrohematos@outlook.com>
2021-09-22 22:12:28 -0300

    Created TransactionsTable

---
+++ src/components/TransactionsTable/index.jsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website development</td>
+            <td className="deposit">$5.000</td>
+            <td>Development</td>
+            <td>02/20/21</td>
+          </tr>
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">-$1.000</td>
+            <td>Housing</td>
+            <td>02/17/21</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
